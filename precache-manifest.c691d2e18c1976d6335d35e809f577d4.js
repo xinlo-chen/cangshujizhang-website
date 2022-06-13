f967865718cf13ab073a98d38e39b077d65536a4
xinlo <1311257306@qq.com>
2022-06-14 01:12:03 +0800

    deploy

---
+++ precache-manifest.c691d2e18c1976d6335d35e809f577d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfca706abec3354bdad5",
+    "url": "/cangshujizhang-website/css/app.6f26a7de.css"
+  },
+  {
+    "revision": "f152f264544dea013a8b34ada5cb16db",
+    "url": "/cangshujizhang-website/index.html"
+  },
+  {
+    "revision": "dfca706abec3354bdad5",
+    "url": "/cangshujizhang-website/js/app.ca0a6803.js"
+  },
+  {
+    "revision": "c79a534ed51eee9fff76",
+    "url": "/cangshujizhang-website/js/chunk-vendors.12888ba8.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/cangshujizhang-website/manifest.json"
+  },
+  {
+    "revision": "e7ea691914d46ff800afb9e97f0cff3a",
+    "url": "/cangshujizhang-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cangshujizhang-website/robots.txt"
+  }
+]);
